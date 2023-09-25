4d6fb1129d2138b6bbe5c83840987996a1a58cde
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-09-25 20:01:17 +0330

    Sync some styles

--- Components/Layout/Layout.tsx
+++ Components/Layout/Layout.tsx
@@ -5,11 +5,16 @@ interface Props {
     children: ReactNode
 }
 
+const mainStyles = {
+    backgroundColor: 'var(--bg-color)',
+    minHeight: 'calc(100vh - 4em)'
+}
+
 const Layout: FC<Props> = ({ children }) => {
     return (
         <>
             <Navbar />
-            <main>
+            <main style={mainStyles}>
                 {children}
             </main>
         </>
