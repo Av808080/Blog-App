3ae645c12c998a276f1662e7be4d2e077ddb47bf
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-09-28 21:10:18 +0330

    Optimize the bundle size of react-syntax-highlighter

--- Components/posts/PostDetail/PostContent.tsx
+++ Components/posts/PostDetail/PostContent.tsx
@@ -4,11 +4,12 @@ import styles from './postContent.module.scss'
 import PostHeader from './PostHeader'
 import ReactMarkdown from 'react-markdown'
 import { SinglePost } from '@/Types/Type'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import coldarkDark from 'react-syntax-highlighter/dist/cjs/styles/prism/coldark-dark'
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
 
 const PostContent: FC<SinglePost> = ({ post }) => {
+    SyntaxHighlighter.registerLanguage('js', js)
     const customComponents = {
         img: ({ ...props }) => {
             return <Image className={styles.image} src={`/Images/Posts/${props.src}`} alt={props.alt} width={600} height={300} />
